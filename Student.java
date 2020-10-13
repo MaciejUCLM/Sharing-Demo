4089b580438716bf0fb58c6521d315f8049f232b
MaciejUCLM <maciej.nalepa@alu.uclm.es>
2020-10-13 20:23:01 +0200

    Class Student v0.1

---
+++ Student.java
@@ -0,0 +1,18 @@
+class Student {
+	int mAge;
+	String mName;
+
+	Student(int age, String name) {
+		mAge = age;
+		mName = name;
+	}
+
+	public int getAge() {
+		return mAge;
+	}
+
+	public String getName() {
+		return mName;
+	}
+}
+
